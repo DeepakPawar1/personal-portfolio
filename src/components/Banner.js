a246efb810bfd37dce6f4cc52877e0df6a4b2329
Pawar, Deepak Atmaram <deepawar@deloitte.com>
2023-02-28 14:44:14 +0530

    Vercel analytics added

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -50,7 +50,7 @@ return (
                     {/* <span className="wrap">{text}</span> */}
                     </h1>
                     <div style={{textAlign:"justify", fontFamily:"monospace"}}>
-                    <p >I am a (so called) <b>Computer Engineer</b> graduated in 2018. I have had a chance to work with multiple firms since then. I have been extensively working on Backend , Frontend and DevOps as per the requirements of the project.</p><p> I have worked as Python Developer for most of my working experience and I have levelled up as Frontend developer after working on series of personal projects, this portfolio website being one of them.  </p></div>
+                    <p >I am a <b>Computer Engineer</b> graduated in 2018. I have had a chance to work with multiple firms since then. I have been extensively working on Backend , Frontend and DevOps as per the requirements of the project.</p><p> I have worked as Python Developer for most of my working experience and I have levelled up as Frontend developer after working on series of personal projects, this portfolio website being one of them.  </p></div>
                     <button onClick={()=> console.log('connect')} style={{backgroundColor:"Black"}}>
                             Lets Connect <ArrowRightCircle size={25}/>
                     </button>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,21 @@
 import { Container,Row,Col } from "react-bootstrap"
 import {DevSvg} from "./SvgIcon";
+import navIcon1 from '../assets/img/nav-icon1.svg';
+import navIcon2 from '../assets/img/nav-icon2.svg';
+import navIcon3 from '../assets/img/nav-icon3.svg';
+import whatsapp from '../assets/img/whatsapp.svg';
 export const Footer =()=>{
 
     return (
         <footer className="footer">
         <Container>
             <Row>
-                <Col sm={6}>
-                    <DevSvg/>
+                <Col sm={6} className={"text-center text-sm-end"}>
+                    <div className="social-icon">
+                        <a href="https://api.whatsapp.com/send?phone=8180935220"><img src={whatsapp} alt="React Logo"/></a>
+                        <a className="grow" href="https://www.linkedin.com/in/deepak-pawar-566192140/"><img src={navIcon1} alt="React Logo"/></a>
+                    </div>
+                <p>CopyRight 2022. All Rights Reserved </p>
                 </Col>
             </Row>
         </Container>
